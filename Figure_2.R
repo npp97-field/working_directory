6963590fb21112d797696f7e1dd1cbda7d3b78d1
zhangyaonju <zhangyaonju@gmail.com>
2018-07-11 22:28:58 -0400

    only 2 variables for fig 2

--- Figure_2.R
+++ Figure_2.R
@@ -5,6 +5,15 @@ source("/Users/yzhang/Documents/GitHub/Phenology_SIF/graph_ae_tools.R")
 # cropland<-shapefile("/Users/yzhang/Project/SIF_phenology/data/crop_north.shp")
 # projection(cropland)<-longlat
 # repcrop<-spTransform(cropland,ae)
+setwd("/Users/yzhang/Project/SIF_phenology/")
+ncin<-nc_open("./data/North_mcd12c1_landcover1_majority.nc")
+lc_data<-ncvar_get(ncin,"lc1")
+nc_close(ncin)
+lc_data1<-lc_data
+lc_data1[lc_data==12|lc_data==14]<-NA #cropland
+lc_data1[lc_data<1]<-NA
+lc_data1[!is.na(lc_data1)]<-1
+
 
 discrete_col<-rep(drought_discrete[c(1,3,5,7)],each=20)
 
@@ -79,63 +88,60 @@ exportnc<-function(dat,fileout){
 }
 
 
-graph_maximum_lags<-function(dataset,phen="sos"){
-  if (dataset=="rs"){
-    vars = c("tday","tnight",'prec','par')
-    exp_var<-list(expression("T"[day]),expression("T"[night]),expression("Precip."),expression("PAR"))
-  }else{
-    vars = c("temp","prec","par")
-    exp_var<-list(expression("T"[mean]),expression("Precip."),expression("PAR"))
+vars = c("tday",'prec')
+exp_var<-list(expression("T"[day]),expression("Precip.")) ###,expression("T"[night]) ,expression("PAR")
+phen="eos"
+dataset="rs"
+n_col = 1
+n_row = length(vars)  
+lay_out = c(n_col,n_row)
+phen_files<-list.files(paste("/Users/yzhang/Project/SIF_phenology/analysis/correlation_clear_",dataset,"/",sep=""),
+                       pattern=paste("^cor_",phen,"_pre",sep=""),full.names = T)
+pdf(paste("/Users/yzhang/Dropbox/YAOZHANG/paper/2018_SIF_phenology/figures/",phen,"_",
+          dataset,"_max_lag.pdf",sep=''),width=8.5,height=14/4*n_row)
+par(oma=c(0,0,1,1))
+for (v in 1:length(vars)){
+  var_file<-phen_files[grep(vars[v],basename(phen_files))]
+  nc_var_dat<-array(NA,dim=c(720*120,4))
+  nc_var_sig<-array(NA,dim=c(720*120,4))
+  for (i in 1:length(var_file)){
+    ncin<-nc_open(var_file[i])
+    ncdat<-ncvar_get(ncin,"cor_coef")
+    ncsig<-ncvar_get(ncin,"cor_pv")
+    nc_close(ncin)
+    nc_var_dat[,i]<-ncdat
+    nc_var_sig[,i]<-ncsig
   }
-  n_col = 1
-  n_row = length(vars)  
-  lay_out = c(n_col,n_row)
-  phen_files<-list.files(paste("/Users/yzhang/Project/SIF_phenology/analysis/correlation_clear_",dataset,"/",sep=""),
-                         pattern=paste("^cor_",phen,"_pre",sep=""),full.names = T)
-  pdf(paste("/Users/yzhang/Dropbox/YAOZHANG/paper/2018_SIF_phenology/figures/",phen,"_",
-            dataset,"_max_lag.pdf",sep=''),width=8.5,height=14/4*n_row)
-  par(oma=c(1,1,1,1))
-  for (v in 1:length(vars)){
-    var_file<-phen_files[grep(vars[v],basename(phen_files))]
-    nc_var_dat<-array(NA,dim=c(720*120,4))
-    nc_var_sig<-array(NA,dim=c(720*120,4))
-    for (i in 1:length(var_file)){
-      ncin<-nc_open(var_file[i])
-      ncdat<-ncvar_get(ncin,"cor_coef")
-      ncsig<-ncvar_get(ncin,"cor_pv")
-      nc_close(ncin)
-      nc_var_dat[,i]<-ncdat
-      nc_var_sig[,i]<-ncsig
-    }
-    abs_cor<-abs(nc_var_dat)
-    maxindex<-apply(abs_cor,1,which.max)  #### the index where the maximum happens
-    idx <- !(sapply(maxindex, length))
-    maxindex[idx]<-NA
-    maxind<-unlist(maxindex)
-    #
-    sel<-as.matrix(cbind(1:86400,maxind))
-    sign_cor<-sign(nc_var_dat[sel])
-    max_cor<-apply(abs_cor,1,max)*sign_cor
-    max_sig<-nc_var_sig[sel]
-    dim(maxind)<-c(720,120)
-    dim(max_cor)<-c(720,120)
-    dim(max_sig)<-c(720,120)
-    
-    nc_var_ae<-nc2ae(max_cor)
-    nc_max_ae<-nc2ae(maxind)-1
-    nc_sig_ae<-convertraster2points(max_sig<0.05)
-    
-    ##export maximum correaltion
-    # exportnc(max_cor,paste("/Users/yzhang/Project/SIF_phenology/analysis/correlation_clear_",
-    #                        dataset,"/max_correlation_",phen,"_",vars[v],".nc",sep=""))
-    # exportnc(max_sig,paste("/Users/yzhang/Project/SIF_phenology/analysis/correlation_clear_",
-    #                        dataset,"/max_significance_",phen,"_",vars[v],".nc",sep=""))
-    
-    plot_nc_var_sig(nc_var_ae,nc_sig_ae,c(-1,1),exp_var[[v]],notion[v*2-1],rev(ano_col_gmt),
-                    c(1,n_row+1-v),c(2,n_row))
-    plot_nc_var(nc_max_ae,c(-0.5,3.5),exp_var[[v]],notion[v*2],rev(discrete_col),
-                c(2,n_row+1-v),c(2,n_row))
-  }
-  dev.off()
+  abs_cor<-abs(nc_var_dat)
+  maxindex<-apply(abs_cor,1,which.max)  #### the index where the maximum happens
+  idx <- !(sapply(maxindex, length))
+  maxindex[idx]<-NA
+  maxind<-unlist(maxindex)
+  #
+  sel<-as.matrix(cbind(1:86400,maxind))
+  sign_cor<-sign(nc_var_dat[sel])
+  max_cor<-apply(abs_cor,1,max)*sign_cor
+  max_sig<-nc_var_sig[sel]
+  dim(maxind)<-c(720,120)
+  dim(max_cor)<-c(720,120)
+  dim(max_sig)<-c(720,120)
+  
+  nc_var_ae<-nc2ae(max_cor*lc_data1)
+  nc_max_ae<-nc2ae(maxind*lc_data1)-1
+  nc_sig_ae<-convertraster2points(max_sig<0.05*lc_data1)
+  
+  plot_nc_var_sig(nc_var_ae,nc_sig_ae,c(-1,1),exp_var[[v]],notion[v*2-1],rev(ano_col_gmt),
+                  c(1,n_row+1-v),c(2,n_row))
+  plot_nc_var(nc_max_ae,c(-0.5,3.5),exp_var[[v]],notion[v*2],rev(discrete_col),
+              c(2,n_row+1-v),c(2,n_row))
+  data_all<-cbind(as.vector(maxind*lc_data1),as.vector(sign_cor*lc_data1),as.vector(max_sig*lc_data1))
+  data_sign<-data_all[complete.cases(data_all),]
+  #neg_sig<-data_sign[data_sign[,2]<0|data_sign[,3]<0.05,1]
+  pos_sig<-data_sign[data_sign[,2]>0|data_sign[,3]<0.05,1]
+  ct<-table(pos_sig)
+  lbls<-c("","","","")
+  par(fig = c(0.55,0.72,1-v/n_row,1-v/n_row+0.17),new=T)
+  pie(ct,labels = lbls,col=drought_discrete[c(1,3,5,7)],clockwise=T)
 }
-graph_maximum_lags("rs","eos")
+
+dev.off()
