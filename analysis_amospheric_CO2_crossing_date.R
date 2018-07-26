885968b7d0d6d61bb64a61eb55303e0d818b15f8
zhangyaonju <zhangyaonju@gmail.com>
2018-07-26 16:16:15 -0400

    updated to start with 1980 and consistent with Piao et al 2017

--- analysis_amospheric_CO2_crossing_date.R
+++ analysis_amospheric_CO2_crossing_date.R
@@ -2,59 +2,99 @@
 co2_brw<-read.table("/Users/yzhang/Data/CO2_data/co2_brw_surface-insitu_1_ccgg_DailyData.txt",sep=' ')
 names(co2_brw)<-c('site_code','year','month','day','hour','minute','second','value','value_unc','nvalue',
                   'latitude','longitude','altitude','elevation','intake_height','instrument','qcflag')
-co2_brw<-co2_brw[co2_brw$year>=1980&co2_brw$year<=2016,]
+co2_brw<-co2_brw[co2_brw$year>=1975&co2_brw$year<=2016,]
 date_brw<-strptime(paste(co2_brw$year,'-',co2_brw$month,'-',co2_brw$day,sep=""),format="%Y-%m-%d")
 #plot(date_brw,co2_brw$value)
 co2_brw$value[co2_brw$value<0]<-NA
 
 
-co2_mlo<-read.table('/Users/yzhang/Data/CO2_data/co2_mlo_surface-insitu_1_ccgg_DailyData.txt',sep=" ")
-names(co2_mlo)<-c('site_code','year','month','day','hour','minute','second','value','value_unc','nvalue',
-                  'latitude','longitude','altitude','elevation','intake_height','instrument','qcflag')
-co2_mlo<-co2_mlo[co2_mlo$year>=2000&co2_mlo$year<=2016,]
-date_mlo<-strptime(paste(co2_mlo$year,'-',co2_mlo$month,'-',co2_mlo$day,sep=""),format="%Y-%m-%d")
-#points(date_mlo,co2_mlo$value,col='red')
-co2_mlo$value[co2_mlo$value<0]<-NA
-plot(date_brw,co2_brw$value,type='l')
-lines(date_mlo,co2_mlo$value,col='red')
+# co2_mlo<-read.table('/Users/yzhang/Data/CO2_data/co2_mlo_surface-insitu_1_ccgg_DailyData.txt',sep=" ")
+# names(co2_mlo)<-c('site_code','year','month','day','hour','minute','second','value','value_unc','nvalue',
+#                   'latitude','longitude','altitude','elevation','intake_height','instrument','qcflag')
+# co2_mlo<-co2_mlo[co2_mlo$year>=2000&co2_mlo$year<=2016,]
+# date_mlo<-strptime(paste(co2_mlo$year,'-',co2_mlo$month,'-',co2_mlo$day,sep=""),format="%Y-%m-%d")
+# #points(date_mlo,co2_mlo$value,col='red')
+# co2_mlo$value[co2_mlo$value<0]<-NA
+# plot(date_brw,co2_brw$value,type='l')
+# lines(date_mlo,co2_mlo$value,col='red')
 
 
 
 library(TSA)
-ts_brw<-ts(data = co2_brw$value,start = c(1980,1),frequency=365.2425)
+ts_brw<-ts(data = co2_brw$value,start = c(1975,1),frequency=365.2425)
 har_brw<-harmonic(ts_brw,4)
 xt<-(1:length(co2_brw$value))/365.2425
 
+#fit the mean seasonal + interannual trend
 fit_brw<-lm(co2_brw$value~har_brw+poly(xt,4))
 
 plot(date_brw,co2_brw$value,type="l")
 lines(date_brw[!is.na(co2_brw$value)],fit_brw$fitted.values,col='red')
-plot(date_brw[!is.na(co2_brw$value)],fit_brw$residuals,type="l")
-resid<-fit_brw$residuals
-library(latticeExtra)
-library(zoo)
-smoothed_resi<-simpleSmoothTs(resid,c=3,sides=2,width=30)
-lines(date_brw[!is.na(co2_brw$value)],smoothed_resi,col='red')
 
-###add the smoothed residual back to fitted to reconstruct co2
-smoothed_brw<-smoothed_resi+fit_brw$fitted.values
-plot(date_brw[!is.na(co2_brw$value)],smoothed_brw)
+co2_brw$residuals[!is.na(co2_brw$value)]<-fit_brw$residuals
+residual_std<-sd(co2_brw$residuals,na.rm=T)
+threshold<-quantile(co2_brw$residuals,c(0.01,0.99),na.rm=T)
+res<-co2_brw$residuals
+res[res<threshold[1]|res>threshold[2]]<-NA
+co2_brw$residuals_no_outlier<-res
+plot(date_brw,co2_brw$residuals,type="l")
+lines(date_brw,co2_brw$residuals_no_outlier,type="l",col='red')
+
+resid<-co2_brw$residuals_no_outlier
+
+####### this is the function for the gaussian filter with a sigma value
+gaussian_filter<-function(ts,fwhm,cutoff=3){
+  sigma<-fwhm/2.3548
+  size_filter<-round(fwhm/2.3548*cutoff)
+  #generate the kernal
+  kernal<-rep(NA,size_filter*2-1)
+  for (i in 1:(size_filter*2-1)){
+    kernal[i]<-exp((-(i-size_filter)^2)/(2*sigma^2))/(sigma*sqrt(2*pi))
+  }
+  #### apply the kernal
+  ts_filtered<-ts
+  for (i in size_filter:(length(ts)-size_filter+1)){
+    ts_filtered[i]<-sum(ts[(i-size_filter+1):(i+size_filter-1)]*kernal,na.rm=T)/
+      (sum((!is.na(ts[(i-size_filter+1):(i+size_filter-1)]))*kernal))
+  }
+  return(ts_filtered)
+}
+
+
+# smoothed_resi<-simpleSmoothTs(resid,c=3,sides=2,width=30)
+# smoothed_resi<-smth.gaussian(resid,window = 30)
+smoothed_resi<-gaussian_filter(ts = resid,fwhm = 30)
+lines(date_brw,smoothed_resi,col='blue')
 
 fit_coef<-fit_brw$coefficients
-deseasonal<-fit_coef[1]+poly(xt,4)%*%fit_coef[10:13]
-plot(date_brw,deseasonal)
+###add the smoothed residual back to fitted to reconstruct co2
+fitted<-fit_coef[1]+poly(xt,4)%*%fit_coef[10:13]+har_brw%*%fit_coef[2:9]
 
-seasonal = smoothed_brw-deseasonal[!is.na(co2_brw$value)]
-plot(seasonal)
-co2_brw$seasonal[!is.na(co2_brw$value)]<-seasonal
-plot(date_brw,co2_brw$seasonal,type="l")
-co2_brw$no_gap_seasonal<-na.fill(co2_brw$seasonal,fill="extend")
+smoothed_brw<-smoothed_resi+fitted
+deseasonal<-gaussian_filter(c(smoothed_brw,rep(smoothed_brw[(length(smoothed_brw)-364):length(smoothed_brw)],3)), 
+                            fwhm=390)
+deseasonal<-deseasonal[1:length(smoothed_brw)]
 
 
-szc<-c()
-fzc<-c()
+plot(date_brw,smoothed_brw,type='l')
+lines(date_brw,co2_brw$value,col='red')
+lines(date_brw,deseasonal,col="orange")
+plot(date_brw,smoothed_brw-deseasonal)
+fit_coef<-fit_brw$coefficients
+
+
+
+seasonal = smoothed_brw-deseasonal
+plot(date_brw,seasonal)
+co2_brw$seasonal<-seasonal
+
+
+
+
+szc<-rep(NA,37)
+fzc<-rep(NA,37)
 for (y in 1980:2016){
-  dat<-co2_brw$no_gap_seasonal[co2_brw$year==y]
+  dat<-co2_brw$seasonal[co2_brw$year==y]
   eop_start<-length(dat)-which.max(rev(dat)<0)+1
   sop_start<-which.max(dat<0)-1
   fzc[y-1979]<-eop_start+dat[eop_start]/(dat[eop_start]-dat[eop_start+1])
@@ -85,12 +125,12 @@ for (i in 1:16){
 }
 
 
-phen_files<-list.files("/Users/yzhang/Project/SIF_phenology/analysis/pheno_fluxcom/ANN/",full.names = T)
+phen_files<-list.files("/Users/yzhang/Project/SIF_phenology/analysis/pheno_fluxcom/ANN/",full.names = T)[2:35]
 weightedeos_flux<-list()
 weightedsos_flux<-list()
 average_eos_flux<-c()
 average_sos_flux<-c()
-for (i in 1:13){
+for (i in 1:34){
   ncin<-nc_open(phen_files[i])
   if(i ==1){
     thresh<-ncvar_get(ncin,varid="THESHOLD")
@@ -103,39 +143,65 @@ for (i in 1:13){
 
 plot(average_eos*365,ylim=c(200,340))
 plot(szc[1:23],col='red',type="l")
-
+library(pracma)
 pdf("/Users/yzhang/Dropbox/YAOZHANG/paper/2018_SIF_phenology/correlation_between_SOP_EOP_and_crosing_date.pdf",width=8,height=6)
 par(fig=c(0,1,0.5,1),mgp=c(3,0.3,0),mar=c(4,4,0.4,0.4))
-plot(2001:2016,average_sos-mean(average_sos),xlim=c(2001,2016),
-     ylim=c(-15,15),ylab="",xlab="",axes=F,col="blue",lwd=2)
-points(2001:2013,average_sos_flux-mean(average_sos_flux),col="darkgreen",lwd=2)
-lines(2001:2016,szc[22:37]-mean(szc[22:37]),col="red",lwd=2)
-axis(1)
-axis(2,las=2)
+ano_sos<-average_sos-mean(average_sos,na.rm=T)
+ano_flux<-average_sos_flux-mean(average_sos_flux,na.rm=T)
+ano_szc<-szc-mean(szc,na.rm=T)
+
+years<-1980:2016
+plot(years[22:37],ano_sos,xlim=c(1980,2016),
+     ylim=c(-10,10),ylab="",xlab="",axes=F,col="blue",type="l",lwd=2,xaxs="i",yaxs="i")
+lines(years[1:34],ano_flux,col="darkgreen",lwd=2)
+lines(years,szc-mean(szc,na.rm=T),col="red",lwd=2)
+axis(1,at=1980:2016,label=rep("",37),tck=-0.01)
+axis(1,at=0:7*5+1980,tck=-0.03)
+axis(2,las=2,tck=-0.03)
 mtext(side=1,line=2,"Year")
 mtext(side=2,line=2.3,"DOY")
 box()
-text(2013,13,"r=0.48, p=0.059",col="blue")
-text(2013,10,"r=-0.02, p=0.95",col="darkgreen")
-legend("topleft",c("average start of photosynthesis (CSIF)","average start of photosynthesis (FLUXCOM)","spring zero crossing date"),
-       lty=c(NA,NA,1),pch=c(1,1,NA),col=c("blue","darkgreen","red"),bty="n")
+text(2002,13,"r=0.48, p=0.059",col="blue")
+text(2002,10,"r=-0.02, p=0.95",col="darkgreen")
+legend("topright",c("Average SOP (CSIF)","Average SOP (FLUXCOM)","spring zero crossing date"),
+       lty=c(1,1,1),lwd=c(2,2,2),col=c("blue","darkgreen","red"),bty="n")
 cor.test(szc[22:37],average_sos)
-cor.test(szc[22:34],average_sos_flux)
+cor.test(szc[1:34],average_sos_flux)
+##### regression slopes
+reg1<-lm(ano_szc~years)
+lines(years,reg1$fitted.values,col="red",lty=2)
+reg4<-lm(ano_szc[22:37]~years[22:37])
+lines(years[22:37],reg4$fitted.values,col="red",lty=2)
+reg2<-lm(ano_sos~years[22:37])
+lines(years[22:37],reg2$fitted.values,col="blue",lty=2)
+reg3<-lm(ano_flux~years[1:34])
+lines(years[1:34],reg3$fitted.values,col="darkgreen",lty=2)
+##### significance test
+
 
 par(fig=c(0,1,0,0.5),mgp=c(3,0.3,0),mar=c(4,4,0.4,0.4),new=T)
-plot(2001:2016,average_eos-mean(average_eos),xlim=c(2001,2016),
-     ylim=c(-25,25),ylab="",xlab="",axes=F,col="blue",lwd=2)
-points(2001:2013,average_eos_flux-mean(average_eos_flux),col="darkgreen",lwd=2)
-lines(2001:2016,fzc[22:37]-mean(fzc[22:37]),col="red",lwd=2)
-axis(1)
-axis(2,las=2)
+ano_eos<-average_eos-mean(average_eos,na.rm=T)
+ano_eos_flux<-average_eos_flux-mean(average_eos_flux,na.rm=T)
+ano_fzc<-fzc-mean(fzc,na.rm=T)
+plot(2001:2016,ano_eos,xlim=c(1980,2016),
+     ylim=c(-15,15),ylab="",xlab="",axes=F,col="blue",lwd=2,type='l',xaxs="i",yaxs="i")
+lines(1980:2013,ano_eos_flux,col="darkgreen",lwd=2)
+lines(1980:2016,ano_fzc,col="red",lwd=2)
+axis(1,at=1980:2016,label=rep("",37),tck=-0.01)
+axis(1,at=0:7*5+1980,tck=-0.03)
+axis(2,las=2,tck=-0.03)
 mtext(side=1,line=2,"Year")
 mtext(side=2,line=2.3,"DOY")
 box()
 cor.test(fzc[22:37],average_eos)
-
-text(2010,21,"r=-0.15, p=0.59",col="blue")
-text(2010,17,"r=-0.30, p=0.31",col="darkgreen")
+reg1<-lm(ano_fzc~years)
+lines(years,reg1$fitted.values,col="red",lty=2)
+reg2<-lm(ano_eos~years[22:37])
+lines(years[22:37],reg2$fitted.values,col="blue",lty=2)
+reg3<-lm(ano_eos_flux~years[1:34])
+lines(years[1:34],reg3$fitted.values,col="darkgreen",lty=2)
+text(2002,21,"r=-0.15, p=0.59",col="blue")
+text(2002,16,"r=-0.30, p=0.31",col="darkgreen")
 dev.off()
 
 
